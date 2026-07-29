1dfd4572eaecf942f5172dc331fcf1f4cdbe07f2
dadatcha <moreaujules600@gmail.com>
2026-07-30 00:43:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def home():
     return "Je suis en ligne !"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
