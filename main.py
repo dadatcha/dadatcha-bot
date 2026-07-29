b674f1d570dc8f91e0f896bff68434ad2205f08d
dadatcha <moreaujules600@gmail.com>
2026-07-30 01:14:18 +0200

    Add main execution block to main.py

--- main.py
+++ main.py
@@ -6046,6 +6046,7 @@ async def overview(ctx):
     # Association du bouton au message
     view = RestartView(owner_id=ctx.author.id)
     await ctx.send(embed=embed, view=view)
+    
     if __name__ == "__main__":
     keep_alive()
     bot.run(os.environ.get("DISCORD_TOKEN")) # ou ton token direct
